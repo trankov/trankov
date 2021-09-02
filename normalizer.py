8e1c05b1b1dcc0a4d65059404aade29e652a8cfd
Aleksei Trankov <trankov@yandex.ru>
2021-09-02 15:16:41 +0500

    Update normalizer.py

--- normalizer.py
+++ normalizer.py
@@ -1,20 +1,24 @@
-# coding=utf-8
-
-# "Console-formatted" text normalizer
+# Convert line-breaked text to paragraphs.
 
 import re
 
+
+
 COMMON_FLAGS = re.IGNORECASE|re.MULTILINE # = 10
 
+
 def unbreak(n):
-    'Removes non-paragraph line breaks'
+    """Removes non-paragraph line breaks"""
+    
     return re.sub(pattern = r'([^.!?])($\n)',
             repl = r'\g<1> ', 
             string = n, count = 0, 
             flags = COMMON_FLAGS)
 
+
 def typographer(n):
-    'Prettyfier'
+    """Replace ... to … , -- to — (emdash), " -" in line beginning to "— " (emdash and space) """
+    
     repl_set = (
         (r'--', r'—'),
         (r'^-\s', r'^— '),
@@ -22,22 +26,30 @@ def typographer(n):
     )   
     for x in repl_set:
         n = re.sub(x[0], x[1], n, 0, COMMON_FLAGS)
+    
     return n
 
+
 def qouter(n):
-    'Set « » instead of " "'
+    """Set « » instead of " " """
+    
     lquote_match = r'([\s^])"'
     rquote_match = r'"([.,!?:;…\n\s\r$])'
     n = re.sub(rquote_match, r'»\g<1>', n, 0, COMMON_FLAGS)
     n = re.sub(lquote_match, r'\g<1>«', n, 0, COMMON_FLAGS)
     return n
 
+
 def total_prettify(n):
+    """Step-by-step convertion"""
+    
     return unbreak(
         typographer(
             qouter(n)
         )
     )
+
+
 
 if __name__ == '__main__':
     n = '''
