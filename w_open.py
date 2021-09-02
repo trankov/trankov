118e8e59708016fc5a87283c2db8a6aa99b5b442
Aleksei Trankov <trankov@yandex.ru>
2021-09-02 15:41:07 +0500

    Update w_open.py

--- w_open.py
+++ w_open.py
@@ -1,44 +1,50 @@
-# We can read/write just in one line: open(path, 'w').write(str)
-# or var = open(path, 'r').read()
+# We can read/write files just in one line: 
+#
+# >>> open(path, 'w').write(string)
+# or 
+# >>> print(open(path, 'r').read())
 #
 # But there we have a problem with unclosed I/O stream. We cannot
-# close just written file manually.
+# close just readen/written file manually. Even via deep inspection.
 # F.e., see https://bugs.python.org/issue37350
 #
-# That's why all manuals force us to use "with", or assign open() to
-# temporarly variable for closing it manually.
+# Using "with" or assign "open()" to the variable 
+# are not applicable if we need a single-line syntax.
 #
-# But "Flat is better than nested" and "Readability counts".
-# One-line is simpler, flatter and much readable than any other way.
-#
-# To solve this issue I use a small class for wrapping 
-# standard "open" metod and it's write() and read() methods.
-# It works the same as "open", but controls the file for 
+# This class works the same as "open", but controls the file for 
 # it always have to be closed after this methods done.
 #
+# Also it helps to avoid "with" construction making code flatter.
+#
 # Usage:
-# from w_open import w_open
+# from w_open import WOpen
 
-class w_open:
-    "Wraps open().write() and open().read() for keeping files always closed"
+
+class WOpen:
+    """ Wraps open().write() and open().read() for keeping files always closed """
     
     def __init__(self, *args, **kwargs):
         self.f = open(*args, *kwargs)
+        self.is_closed = False
     
     def write(self, src):
         self.f.write(src)
         self.f.close()
-        return self.f.closed
+        if self.f.closed: self.is_closed = True
+        return self.is_closed
     
     def read(self):
         n = self.f.read()
         self.f.close()
+        if self.f.closed: self.is_closed = True
         return n
 
+    
 if __name__ == "__main__":
+    
     print (
-           {True:  "File succesfully closed",
-            False: "File was not closed"} \
-                [w_open('test.txt', 'w').write('wow!')]
+           { True:  "File succesfully written",
+             False: "File was not closed" } [ WOpen('test.txt', 'w').write('wow!') ]
            )
-    print (w_open('test.txt', 'r').read())
+    
+    print ( WOpen('test.txt', 'r').read() )
