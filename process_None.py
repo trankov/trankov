1d458cc841985ed45cb51772555eb6ce366d7a4e
Aleksei Trankov <trankov@yandex.ru>
2022-01-14 05:31:37 +0500

    Update process_None.py

--- process_None.py
+++ process_None.py
@@ -20,3 +20,43 @@ class N:
     prop = 22
     def setprop(self, prop_passed=None):
         self.prop = _None(prop_passed, self.prop or 0)
+
+
+
+# An another approach is better in readability and shorter, but declaration
+# is complex an using is non-intuitive. But it is convenient when there are
+# a lot of checks and they are of different severity. Following example
+# demonstrates check both for None and False value.
+
+
+class Avoid:
+    def __init__(self, value):
+        self.value = value
+
+    def __floordiv__(self, default_value):
+        '''Returns default if None'''
+        if self.value is None:
+            return default_value
+        return self.value
+
+    def __truediv__(self, default_value):
+        '''Returns default if False'''
+        if bool(self.value):
+            return self.value
+        return default_value
+
+
+value = 0
+default_value = 2
+
+print( Avoid(value) // default_value )
+print( Avoid(value) /  default_value )
+
+
+# So, it could be written as:
+# 
+#     value = Avoid(value) / default_value
+# 
+# Why not to Avoid(value, default_value)? First, operator defines severity.
+# We can define another if we need (f.e. for checking is numeric value positive
+# or whatever). Second, here we don't need to remember which argument means what.
