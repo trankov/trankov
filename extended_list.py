7855e0d3d54834a1f8ee0efb9e4b119dae125b23
Aleksei Trankov <trankov@yandex.ru>
2022-04-25 14:12:11 +0500

    Update extended_list.py

--- extended_list.py
+++ extended_list.py
@@ -7,7 +7,7 @@
 # n @ str == ['1', '2', '3']
 
 # n = extlist('abcde')
-# n = ['a', 'b', 'c', 'd', 'e']
+# n == ['a', 'b', 'c', 'd', 'e']
 # n @ str.upper == ['A', 'B', 'C', 'D', 'E']
 
 # Does not transform object itself. Just returns new list.
